122e44ca23fe82efdfff0a6ec91bcc0f78848e42
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-28 21:43:14 +0000

    add custom seed functions

--- src/random.cpp
+++ src/random.cpp
@@ -18,12 +18,39 @@ std::string RandomString(int length)
 	return fin;
 }
 
+std::string RandomString(int length, int seed)
+{
+	std::string fin = std::string();
+	for (int i = 0; i < length; i++)
+	{
+		std::srand((i * 2.9) + seed + time(0));
+		int randint = std::rand() % 94 + 33;
+		char character = randint;
+		fin += character;
+		std::this_thread::sleep_for(std::chrono::milliseconds(8));
+	}
+	return fin;
+}
+
 std::vector<int> RandomInts(int length)
 {
 	std::vector<int> IntArray = std::vector<int>(length);
 	for (int i = 0; i < length; i++)
 	{
 		std::srand((i * 2.9) + i + time(0));
+		int randint = std::rand() % 28;
+		IntArray[i] = randint;
+		std::this_thread::sleep_for(std::chrono::milliseconds(8));
+	}
+	return IntArray;
+}
+
+std::vector<int> RandomInts(int length, int seed)
+{
+	std::vector<int> IntArray = std::vector<int>(length);
+	for (int i = 0; i < length; i++)
+	{
+		std::srand((i * 2.9) + seed + time(0));
 		int randint = std::rand() % 28;
 		IntArray[i] = randint;
 		std::this_thread::sleep_for(std::chrono::milliseconds(8));
